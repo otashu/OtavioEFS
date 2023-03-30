4664cd59bf29cd6195e6d78cbc025cc4a95233d6
root <root@ip-172-31-4-61.ec2.internal>
2023-03-30 11:59:23 +0000

    script pronto, fiz alguns testes no log.

---
+++ ApacheFunciona.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+isActive=$(systemctl is-active httpd.service)
+echo "Olá! O servidor estava ">>/efs/OtavioCosta/logs.txt;
+if [ $isActive == "active" ];
+then
+	echo -e "Funcionando!" >> /efs/OtavioCosta/logs.txt
+else
+	echo -e "Inativo :(">>/efs/OtavioCosta/logs.txt
+fi
